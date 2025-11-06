efa158305d2fdd5d9eaa2c7247bce11b5050e240
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2025-11-06 23:24:01 +0100

    Fix: Remove setuptools deprecation warnings
    - Remove deprecated license classifier in favor of SPDX format
    - Simplify setup.py to defer to pyproject.toml
    - Add license-files configuration
    - Update setuptools requirement to >=61.0
    - All builds now clean without warnings

--- setup.py
+++ setup.py
@@ -1,35 +1,6 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# All configuration is now in pyproject.toml
+# This file is kept for backward compatibility with older tools
+setup()
 
-setup(
-    name='webssh-sh',
-    version='22.11.8',
-    description='Shell Helpers about WebSSH',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/isontheline/webssh.sh',
-    author='Arnaud MENGUS',
-    license='MIT',
-    packages=['wsh'],
-    install_requires=[
-        'importlib-metadata>=1.0; python_version<"3.8"',
-    ],
-    scripts=[],
-    entry_points={
-        'console_scripts': ['wshcopy=wsh.wshcopy:cli'],
-    },
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-    ],
-    python_requires='>=3.8',
-)
